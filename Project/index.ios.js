43f0cf3e5416f90c86a0be84be4f8f5548c5b9e5
Siam <shouyuzhu@hotmail.com>
2016-06-16 17:57:08 +0800

    Add all the resource images

--- Project/index.ios.js
+++ Project/index.ios.js
@@ -9,24 +9,65 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TabBarIOS,
 } from 'react-native';
 
 class WYNewsRN extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuItems: [{
+        title: '新闻',
+        icon: require('./App/resources/night_tabbar_icon_news_normal.png'),
+        highlight: 'night_tabbar_icon_news_normal',
+        tag: 'news'
+      },
+      {
+        title: '阅读',
+        icon: require('./App/resources/night_tabbar_icon_reader_normal.png'),
+        highlight: 'night_tabbar_icon_news_normal',
+        tag: 'reading'
+      },
+      {
+        title: '视频',
+        icon: require('./App/resources/night_tabbar_icon_media_normal.png'),
+        highlight: 'night_tabbar_icon_news_normal',
+        tag: 'media'
+      },
+      {
+        title: '话题',
+        icon: require('./App/resources/night_tabbar_icon_bar_normal.png'),
+        highlight: 'night_tabbar_icon_news_normal',
+        tag: 'bar'
+      },
+      {
+        title: '我',
+        icon: require('./App/resources/night_tabbar_icon_me_normal.png'),
+        highlight: 'night_tabbar_icon_news_normal',
+        tag: 'me'
+      }],
+      selectedTag: 'news'
+    };
+  }
   render() {
+    var items = this.state.menuItems.map((itemValue) => {
+      return (
+        <TabBarIOS.Item
+        key = {itemValue.tag}
+        title = {itemValue.title}
+        icon = {itemValue.icon}
+        >
+        </TabBarIOS.Item>
+      )
+    });
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TabBarIOS
+      unselectedTintColor = 'gray'
+      tintColor = 'red'
+      barTintColor = 'white'>
+      {items}
+      </TabBarIOS>
     );
   }
 }
